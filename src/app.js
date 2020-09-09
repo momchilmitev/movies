3aa7b0e9171aad36e1091b12a6ade7e6c6a49b88
Momchil Mitev <momchilmitev87@gmail.com>
2020-09-09 21:26:51 +0200

    added controllers and the routing to them

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+import home from './controllers/home.js';
+import register from './controllers/register.js';
+import login from './controllers/login.js';
+import catalog, { create, edit, details } from './controllers/movies.js';
+
 window.addEventListener('load', () => {
 	const app = Sammy('#container', function () {
 		this.use('Handlebars', 'hbs');
@@ -5,16 +10,13 @@ window.addEventListener('load', () => {
 		this.get('/', home);
 		this.get('index.html', home);
 		this.get('#/home', home);
+		this.get('#/register', register);
+		this.get('#/login', login);
+		this.get('#/catalog', catalog);
+		this.get('#/create', create);
+		this.get('#/edit/:id', edit);
+		this.get('#/details/:id', details);
 	});
 
 	app.run();
-
-	async function home() {
-		this.partials = {
-			header: await this.load('../templates/common/header.hbs'),
-			footer: await this.load('../templates/common/footer.hbs'),
-		};
-
-		this.partial('../templates/home.hbs');
-	}
 });
